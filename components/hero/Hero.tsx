31e3f832ec1d93e358d931d2b2a1c8debaeb39ab
arselarah <arselarah@gmail.com>
2025-05-27 15:17:31 -0600

    Cambios antes de revisión

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,51 +1,73 @@
 import React from 'react'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/300.css'
+import '@fontsource/poppins/200.css'
+import '@fontsource/poppins/100.css'
 
 export default function Hero() {
   return (
     <>
       <div className='flex min-h-screen w-full bg-black'>
-        <div className='mx-auto flex w-full max-w-[100rem] flex-col items-start justify-between'>
-          <div className='w-[40vh]'></div>
-          <div>
+        <div className='mx-auto flex w-full max-w-[100rem] flex-col items-start justify-between gap-8'>
+          <div className='h-1/4 min-h-80 w-full'></div>
+          <div className='flex flex-col gap-4 px-4'>
             <div>
-              <h1 className='text-white'>Impulso en Imagen</h1>
+              <h1 className='text-clamp_cover leading-tight text-white'>
+                REDZPOT
+              </h1>
+              <h1 className='text-clamp_cover leading-tight text-white'>
+                Impulso en Imagen
+              </h1>
             </div>
             <div>
-              <button className='text-white'>Contáctanos</button>
+              <button className='border-redzpot border-2 px-12 py-3 text-sm text-white'>
+                Contáctanos
+              </button>
             </div>
           </div>
-          <div className='cajas w-full'>
-            <div className='flex w-full flex-col gap-8 md:flex-row'>
-              <div className='caja flex aspect-square flex-col justify-between bg-red-600 p-8 md:w-1/3'>
-                <div className='caja_icono'></div>
-                <div className='caja_titulo text-white'>
-                  <h3>Servicios 1</h3>
+          <div className='cajas w-full font-poppins'>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+              <div className='caja bg-redzpot flex aspect-square flex-col justify-between p-8'>
+                <div className='caja_icono min-h-[40%]'></div>
+                <div className='caja_titulo'>
+                  <h3 className='text-clamp_subtitles font-light text-white'>
+                    Pantalla Vial
+                  </h3>
                 </div>
                 <div className='caja_texto text-white'>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <p className='text-clamp_textoImportante'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    vitae lorem dapibus, euismod est ut, egestas lacus.
                   </p>
                 </div>
               </div>
-              <div className='caja flex aspect-square flex-col justify-between bg-slate-900 p-8 md:w-1/3'>
-                <div className='caja_icono'></div>
-                <div className='caja_titulo text-white'>
-                  <h3>Servicios 2</h3>
+              <div className='caja bg-secondaryDark flex aspect-square flex-col justify-between p-8'>
+                <div className='caja_icono min-h-[40%]'></div>
+                <div className='caja_titulo'>
+                  <h3 className='text-clamp_subtitles font-light text-white'>
+                    Espectaculares
+                  </h3>
                 </div>
                 <div className='caja_texto text-white'>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <p className='text-clamp_textoImportante'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    vitae lorem dapibus, euismod est ut, egestas lacus.
                   </p>
                 </div>
               </div>
-              <div className='caja flex aspect-square flex-col justify-between bg-slate-900 p-8 md:w-1/3'>
-                <div className='caja_icono'></div>
-                <div className='caja_titulo text-white'>
-                  <h3>Servicios 3</h3>
+              <div className='caja bg-secondaryDark barras-rojas relative flex aspect-square flex-col justify-between p-8 md:col-span-2 md:aspect-video lg:col-span-1 lg:aspect-square'>
+                <div className='caja_icono min-h-[40%]'></div>
+                <div className='caja_titulo'>
+                  <h3 className='text-clamp_subtitles font-light text-white'>
+                    Comunicación Interna
+                  </h3>
                 </div>
                 <div className='caja_texto text-white'>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <p className='text-clamp_textoImportante'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    vitae lorem dapibus, euismod est ut, egestas lacus.
                   </p>
                 </div>
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,17 @@ export default {
     extend: {
       colors: {
         background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        foreground: 'var(--foreground)',
+        redzpot: '#df002f',
+        secondaryDark: '#1a1a1a'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
+      },
+      fontSize: {
+        clamp_cover: 'clamp(2rem, 7vw, 4.5rem)',
+        clamp_subtitles: 'clamp(1.2rem, 4vw, 1.8rem)',
+        clamp_textoImportante: 'clamp(1.15rem, 2vw, 1.35rem)'
       }
     }
   },
