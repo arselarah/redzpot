6658b20c00cd3db9beb271c1aa7f2edc3c1f4f17
arselarah <arselarah@gmail.com>
2025-06-13 18:51:14 -0600

    Cambios viernes

--- components/nosotros/Nosotros.tsx
+++ components/nosotros/Nosotros.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Nosotros() {
   return (
@@ -31,10 +32,61 @@ export default function Nosotros() {
           </div>
         </div>
         <article className='contenedor-grid mx-auto flex h-auto w-full max-w-[100rem] flex-col items-start justify-start gap-4 px-4 pb-12 md:px-8 md:pb-16 lg:pb-32'>
-          <div className='relative grid min-h-[50vh] w-full grid-cols-2 grid-rows-3 gap-4 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-4'>
-            <div className='barras-rojas-2 relative col-span-2 row-span-1 aspect-square bg-slate-400 md:row-span-2 lg:row-span-4'></div>
-            <div className='barras-rojas-3 relative col-span-2 row-span-1 aspect-square bg-slate-500 lg:col-span-1 lg:row-span-2'></div>
-            <div className='barras-rojas-4 relative col-span-2 row-span-1 aspect-square bg-slate-600 lg:col-span-1 lg:row-span-2'></div>
+          <div className='relative grid h-auto w-full grid-cols-2 grid-rows-3 gap-4 md:grid-rows-2 md:gap-8 lg:max-h-[50rem] lg:grid-cols-3 lg:grid-rows-4'>
+            <div className='barras-rojas-2 group relative col-span-2 row-span-1 md:row-span-2 lg:row-span-4'>
+              <div className='relative h-full w-full overflow-hidden'>
+                <Image
+                  src='/assets/slider/7.webp'
+                  alt='Imagen de nosotros'
+                  width={500}
+                  height={500}
+                  className='h-full w-full object-cover'
+                />
+                <div className='absolute inset-0 left-0 top-0 flex flex-col items-end justify-end bg-gradient-to-t from-black/90 via-black/30 to-transparent'>
+                  <div className='max-w-80 -translate-x-24 p-10 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100'>
+                    <h4 className='text-fluid-md font-light text-white'>
+                      Lorem ipsum dolor sit amet.
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='barras-rojas-3 group relative col-span-2 row-span-1 lg:col-span-1 lg:row-span-2'>
+              <div className='relative h-full w-full overflow-hidden'>
+                <Image
+                  src='/assets/slider/5.webp'
+                  alt='Imagen de nosotros'
+                  width={500}
+                  height={500}
+                  className='h-full w-full object-cover'
+                />
+                <div className='absolute inset-0 left-0 top-0 flex flex-col items-end justify-end bg-gradient-to-t from-black/90 via-black/30 to-transparent'>
+                  <div className='max-w-80 -translate-x-24 p-10 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100'>
+                    <h4 className='text-fluid-md font-light text-white'>
+                      Lorem ipsum dolor sit amet.
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='barras-rojas-4 group relative col-span-2 row-span-1 lg:col-span-1 lg:row-span-2'>
+              <div className='relative h-full w-full overflow-hidden'>
+                <Image
+                  src='/assets/slider/2.webp'
+                  alt='Imagen de nosotros'
+                  width={500}
+                  height={500}
+                  className='h-full w-full object-cover'
+                />
+                <div className='absolute inset-0 left-0 top-0 flex flex-col items-end justify-end bg-gradient-to-t from-black/90 via-black/30 to-transparent'>
+                  <div className='max-w-80 -translate-x-24 p-10 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:opacity-100'>
+                    <h4 className='text-fluid-md font-light text-white'>
+                      Lorem ipsum dolor sit amet.
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </article>
       </section>
